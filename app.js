3a6eb179a57046cf2646976a13c67ca84190efce
Sean Hinds <sthinds144@gmail.com>
2017-11-06 19:16:46 -0600

    added angular

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function(request, response) {
+    response.render('home');
+});
+
+app.get('/about', function(request, response) {
+    response.render('about');
+});
+
+app.get('/beerStyle', function(request, response) {
+    response.render('beerStyle');
+});
+
+app.get('/contact', function(request, response) {
+    response.render('contact');
+});
+
+app.get('/process', function(request, response) {
+    response.render('process');
+});
+
+app.get('/references', function(request, response) {
+    response.render('references');
+});
+
+app.get('/resources', function(request, response) {
+    response.render('resources');
+});
+
+app.use(function(request, response) {
+    response.status(404);
+    response.render('404');
+});
+
+app.use(function(error, request, response, next) {
+    console.error(err.stack);
+    response.type('plain/text');
+    response.status(500);
+    response.render('500');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ public/js/myAngular.js
@@ -0,0 +1,96 @@
+var myAngular = angular.module('myAngular', []);
+
+myAngular.controller('ctrl1', function($scope) {
+
+  $scope.first = 1;
+  $scope.second = 1;
+
+});
+
+myAngular.controller('mousePop', function($scope) {
+    
+    var targets =  
+        [["lFirst", "pFirst"], ["lSecond", "pSecond"], ["lThird",     "pThird"], ["lFourth", "pFourth"], ["lFifth", "pFifth"], ["lSixth", "pSixth"], ["lSeventh", "pSeventh"], ["lEigth", "pEighth"], ["lNinth", "pNinth"], ["lTenth", "pTenth"], 
+        ["lRes1", "pRes1"], ["lRes2", "pRes2"], ["lRes3", "pRes3"], ["lRes4", "pRes4"], ["lRes5", "pRes5"], ["lRes6", "pRes6"], ["lRes7", "pRes7"], ["lRes8", "pRes8"], ["lRes9", "pRes9"], ["lRes10", "pRes10"]];
+
+    $scope.mouseOver = function(event) {
+	    var el = event.target;
+        var i;
+        for (var j = 0; j < targets.length; j++) {
+            if (targets[j][0] == el.id) {
+                i = j;
+            }
+        }
+        var tar = document.getElementById(targets[i][1]);
+        tar.style.display = "inline-block";
+
+    };
+    
+    $scope.mouseOff = function(event) {
+        var el = event.target;
+        var i;
+        for (var j = 0; j < targets.length; j++) {
+            if (targets[j][0] == el.id) {
+                i = j;
+            }
+        }
+        var tar = document.getElementById(targets[i][1]);
+        tar.style.display = "none";
+        event.stopPropagation;
+    }
+    
+});
+
+myAngular.controller('sel', function($scope) {
+
+    var targs = [
+        {strId: "Pilsener", strTarg: "pilsener"},
+        {strId: "India Pale Ale", strTarg: "indiaPaleAle"},
+        {strId: "Brown Ale", strTarg: "brownAle"},
+        {strId: "Porter", strTarg: "porter"},
+        {strId: "Stout", strTarg: "stout"},
+        {strId: "Oktoberfest", strTarg: "oktoberfest"},
+        {strId: "Lambic", strTarg: "lambic"},
+        {strId: "Saison", strTarg: "saison"},
+        {strId: "Bock", strTarg: "bock"},
+        {strId: "Gose", strTarg: "gose"}
+    ];
+
+    var beers = document.getElementById("selectBox");
+
+    beers.addEventListener("click", function() {
+
+        for (var k = 0; k < targs.length; k++) {
+            console.log("hello");
+            var del = document.getElementById(targs[k].strTarg);
+            del.style.display = "none";
+        }
+
+        var tex = qStrFromSelect(beers);
+        console.log(tex);
+        var i;
+        for (var j = 0; j < targs.length; j++) {
+            if (targs[j].strId === tex) {
+                i = j;
+            }
+        }
+        console.log(i);
+        var tPan = document.getElementById(targs[i].strTarg);
+        tPan.style.display = "inline-block";
+
+
+
+    });
+
+    function qStrFromSelect(box) {
+
+        if (box.selectedIndex == -1) {
+
+            return null;
+
+        }
+
+        return box.options[box.selectedIndex].text;
+
+    }
+});

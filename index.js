a2cb19a61351689cf42812ebbc41c918a2c97c61
Sean Hinds <sthinds144@gmail.com>
2017-11-06 18:26:19 -0600

    5

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-	response.send(cool());
+  response.render('home');
 });
 
 app.get('/db', function (request, response) {

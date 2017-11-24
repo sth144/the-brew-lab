9033f8402cf00e01e833b78bfa95972dc296d72b
Sean Hinds <sthinds144@gmail.com>
2017-11-24 16:30:34 -0600

    config template

--- app.js
+++ app.js
@@ -1,65 +1,112 @@
-/**************************************************************
+/*******************************************************************************************
     Title: Brew Lab Node Index
     Author: Sean Hinds
     Date: 11/16/17
     Description: This node.js app serves as the index for the
         node express handlebars templating engine used to serve
         views for the Brew Lab website.
-**************************************************************/
+*******************************************************************************************/
+
+
+// import express and store in variable. Store express.express() in app variable
 
 var express = require('express');
 var app = express();
+
+
+// default layout is main.hbs
+
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+
+// for running node server locally for development. App uses port 5000
 
 app.set('port', (process.env.PORT || 5000));
 
+
+// the app will look for all static files in public/
+
 app.use(express.static(__dirname + '/public'));
 
+
 // views is directory for all template files
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+
+// render the home page if no url appendage
 
 app.get('/', function(request, response) {
     response.render('home');
 });
 
+
+// render the home page when redirected to homepage
+
 app.get('/home', function(request, response) {
     response.render('home');
 });
+
+
+// render the about page
 
 app.get('/about', function(request, response) {
     response.render('about');
 });
 
+
+// render the beerStyle page
+
 app.get('/beerStyle', function(request, response) {
     response.render('beerStyle');
 });
+
+
+// render the contact page
 
 app.get('/contact', function(request, response) {
     response.render('contact');
 });
 
+
+// render the equipment page
+
 app.get('/equipment', function(request, response) {
     response.render('equipment');
 });
+
+
+// render the process page
 
 app.get('/process', function(request, response) {
     response.render('process');
 });
 
+
+// render the references page
+
 app.get('/references', function(request, response) {
     response.render('references');
 });
+
+
+// render the resources page
 
 app.get('/resources', function(request, response) {
     response.render('resources');
 });
 
+
+// render the 404 resource not found page
+
 app.use(function(request, response) {
     response.status(404);
     response.render('404');
 });
+
+
+// render the 500 page for server errors
 
 app.use(function(error, request, response, next) {
     console.error(err.stack);
@@ -67,6 +114,9 @@ app.use(function(error, request, response, next) {
     response.status(500);
     response.render('500');
 });
+
+
+// display a string on the node console to verify that the app server is running
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ config.template.js
@@ -0,0 +1,10 @@
+module.exports = {
+// copy this to your config file
+    client:     'mysql',
+    connection: {
+      host:     '',
+      database: '',
+      user:     '',
+      password: '',
+    }
+};

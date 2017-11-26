def980c656e71bfae967b97112279e460f97dc65
Sean Hinds <sthinds144@gmail.com>
2017-11-25 20:02:52 -0600

    added to app.json

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ app.use(express.static(__dirname + '/public'));
 
 // import MySQL pool 
 
-var mysql = require('mysql');
-var pool = mysql.createPool({
+var mysql = require('./dbConfig.js');
+/*var pool = mysql.createPool({
     connectionLimit: 10,
     host:     'us-cdbr-iron-east-05.cleardb.net',
     user:     'b3a90fb3ed8ef5',
     password: '1193d107',
     database: 'heroku_318ae3993b1809c'
-});
+});*/
 
 
 // views is directory for all template files

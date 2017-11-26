ff0815248d8fa5a6dabf268fb465bf3963f05940
Sean Hinds <sthinds144@gmail.com>
2017-11-26 09:14:50 -0600

    Secure branch

--- dbConfig.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-	connectionLimit: 10,
-	host:     'us-cdbr-iron-east-05.cleardb.net',
-	user:     'b3a90fb3ed8ef5',
-	password: '1193d107',
-	database: 'heroku_318ae3993b1809c'
-});
-module.exports.pool = pool;

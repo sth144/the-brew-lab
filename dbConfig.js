41c677d3d3018149b4968a4fab02424ca3c36057
Sean Hinds <sthinds144@gmail.com>
2017-12-17 17:58:20 -0600

    secured

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
 	connectionLimit: 10,
-	host:     'us-cdbr-iron-east-05.cleardb.net',
-	user:     'b3a90fb3ed8ef5',
-	password: '1193d107',
-	database: 'heroku_318ae3993b1809c'
+	host:     '',
+	user:     '',
+	password: '',
+	database: ''
 });
 module.exports.pool = pool;
